1adfdaa62c25030b3f9bea206380fa0eaea6da5e
GabrielXd-Tech <seu_email@example.com>
2025-04-10 20:53:32 -0300

    first commit

---
+++ aula1.py
@@ -0,0 +1,10 @@
+"""
+DocString
+Não é um comentário pois o algoritmo lê, guarda na memória mas não realiza nada.
+"""
+'''Pode ser utilizado para criar somente uma linha'''
+
+# Permite escrever um comentário
+print(123) # Na frente
+# Abaixo
+print(34) 
---
+++ aula2.py
@@ -0,0 +1,5 @@
+# \r\n -> CRLF
+# \n -> LF
+print(12, 34, sep="-", end='\n#')# a Virgula separa os números
+print(56, 78, sep='-')# O SEP tem como função definir oq vai entre os espaços.
+print(9, 10, sep='-')
