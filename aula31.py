f1a58b236fde2fb43058fb54b312175bd46475da
GabrielXd-Tech <seu_email@example.com>
2025-04-14 08:00:19 -0300

    aa

--- aula31.py
+++ aula31.py
@@ -8,9 +8,17 @@ id = Identidade
 v1 = 'a'
 print(id(v1))
 
-# condicao = False
+condicao = False
+passou_no_if = None
 
-# if condicao:
-    # print('faça algo')
-# else:
-    # print('Não faça algo')
+if condicao:
+     passou_no_if = True
+     print('faça algo')
+else:
+    print('Não faça algo')
+
+if passou_no_if is None:
+    print('Não passou no if')
+
+if passou_no_if is not None:
+    print('Passou no if')
